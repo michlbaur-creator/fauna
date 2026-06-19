bc1db363d5f387c134cbb7b7be8b2c5ae7922c31
michlbaur-creator <michlbaur@googlemail.com>
2026-06-19 11:18:54 +0200

    10 weitere Arten mit Tafel, Foto und Text; Stammbaum und Bestimmungsschlüssel erweitert

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
    Nur wenn sich Bilder ändern, zusätzlich die MEDIA-Version erhöhen.
 */
 
-const SHELL_CACHE = 'fauna-shell-v5';
+const SHELL_CACHE = 'fauna-shell-v6';
 const MEDIA_CACHE = 'fauna-media-v1';
 const MANIFEST_URL = './offline-manifest.json';
 
