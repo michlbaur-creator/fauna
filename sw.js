38fb13b34ee0f5af4eb29188425cc25de6b2e8b0
michlbaur-creator <michlbaur@googlemail.com>
2026-06-20 17:20:05 +0200

    Design

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
    Nur wenn sich Bilder ändern, zusätzlich die MEDIA-Version erhöhen.
 */
 
-const SHELL_CACHE = 'fauna-shell-v14';
+const SHELL_CACHE = 'fauna-shell-v15';
 const MEDIA_CACHE = 'fauna-media-v1';
 const MANIFEST_URL = './offline-manifest.json';
 
