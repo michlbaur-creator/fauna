103a77598a58fc903dea501c520f481a95c36754
michlbaur-creator <michlbaur@googlemail.com>
2026-06-20 08:03:21 +0200

    Verwandlung-Kachel ausspielen (SW v9) + Karten-Beschriftungen verkleinert

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
    Nur wenn sich Bilder ändern, zusätzlich die MEDIA-Version erhöhen.
 */
 
-const SHELL_CACHE = 'fauna-shell-v8';
+const SHELL_CACHE = 'fauna-shell-v9';
 const MEDIA_CACHE = 'fauna-media-v1';
 const MANIFEST_URL = './offline-manifest.json';
 
