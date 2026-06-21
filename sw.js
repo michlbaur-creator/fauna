b8736901d3589366cad415db329addded84c24bc
michlbaur-creator <michlbaur@googlemail.com>
2026-06-21 11:44:58 +0200

    Verwandlung: Zuordnen-Station + Abschluss-Quiz, Datei jetzt repo-eigen + SW v24

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
    Nur wenn sich Bilder ändern, zusätzlich die MEDIA-Version erhöhen.
 */
 
-const SHELL_CACHE = 'fauna-shell-v22';
+const SHELL_CACHE = 'fauna-shell-v24';
 const MEDIA_CACHE = 'fauna-media-v2';
 const MANIFEST_URL = './offline-manifest.json';
 
