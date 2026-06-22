a2773c519444d5b69f8c6a40985ece26ed677a2e
michlbaur-creator <michlbaur@googlemail.com>
2026-06-22 08:15:11 +0200

    Footer umsortiert, Schwester-Chip, Unterseiten-Footer verlinkt + SW v37

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
    Nur wenn sich Bilder ändern, zusätzlich die MEDIA-Version erhöhen.
 */
 
-const SHELL_CACHE = 'fauna-shell-v36';
+const SHELL_CACHE = 'fauna-shell-v37';
 const MEDIA_CACHE = 'fauna-media-v3';
 const MANIFEST_URL = './offline-manifest.json';
 
