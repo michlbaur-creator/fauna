c77f9c0ddcb8be80d224e56925adb670b3b13fd8
michlbaur-creator <michlbaur@googlemail.com>
2026-06-23 15:31:55 +0200

    Lebensraum: Punktestand zurück, Kachel-Text gekürzt + SW v44

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
    Nur wenn sich Bilder ändern, zusätzlich die MEDIA-Version erhöhen.
 */
 
-const SHELL_CACHE = 'fauna-shell-v42';
+const SHELL_CACHE = 'fauna-shell-v44';
 const MEDIA_CACHE = 'fauna-media-v3';
 const MANIFEST_URL = './offline-manifest.json';
 
